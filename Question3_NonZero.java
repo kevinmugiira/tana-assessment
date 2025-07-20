a24c58d14b28dd4f2d127e89b87a90cc0cb9447c
kevinmugiira <kevinmugiira@gmail.com>
2025-07-20 21:22:17 +0300

    added some comments for description purposes

--- Question3_NonZero.java
+++ Question3_NonZero.java
@@ -2,35 +2,20 @@ import java.util.HashMap;
 
 public class Question3_NonZero {
 
-//    public HashMap<Integer, Integer> allZero(int[] myArr) {
-//        if (myArr == null) {return null;}
-//        HashMap<Integer, Integer> map = new HashMap<>();
-//
-//
-//        for (int i = 1; i < myArr.length; i++) {
-//            if (myArr[i] - myArr[i - 1] == 0 ) {
-//                myArr[i] = 0;
-//                map.put(i, 1); //map.getOrDefault(i, 0) + 1);
-//                return map.get(i);
-//            } else {
-//                map.put(i, 0);
-//                return map.get(i);
-//            }
-//        }
-//        return map;
-//    }
 
 
-    public int turnToZero(int[] myArr) {
-        if (myArr.length < 1) {
-            return 0; // Early return for null input
+    public int turnZero(int[] arr) {
+        if (arr.length < 1) {
+            // return early for null
+            return 0; 
         }
-        int indexTotal = myArr[0];
-        for (int w = 1; w < myArr.length; w++) {
-            if (myArr[w] > indexTotal) {
+       // iterate while checking each element is greater than the sumTotal and if not add it to sumTotal
+        int sumTotal = arr[0];
+        for (int w = 1; w < arr.length; w++) {
+            if (arr[w] > sumTotal) {
                 return 1;
             }
-            indexTotal = indexTotal + myArr[w];
+            sumTotal = sumTotal + arr[w];
         }
         return 0;
 
@@ -38,7 +23,7 @@ public class Question3_NonZero {
 
     public static void main(String[] args) {
         Question3_NonZero q = new Question3_NonZero();
-        System.out.println("output: " + q.turnToZero(new int[]{1,2,3}));
+        System.out.println("output: " + q.turnZero(new int[]{1,2,3}));
     }
 }
 
--- Question4_FirstUniqueProd.java
+++ Question4_FirstUniqueProd.java
@@ -3,7 +3,12 @@ import java.util.HashMap;
 
 public class Question4_FirstUniqueProd {
 
-    public String firstUniqueProduct(String[] products) {
+
+    /*
+      iterate the products array and get the number of times each product appears and store this in a map
+      then via another iteration go through the map and check/get the first one and return it
+     */
+    public String firstUniqueProd(String[] products) {
         if (products.length == 0 || products.length == 1) {
             System.out.println("Invalid array input");
             return null;
@@ -12,10 +17,12 @@ public class Question4_FirstUniqueProd {
         HashMap<String, Integer> dupsMap = new HashMap<>();
         ArrayList<String> dupsList = new ArrayList<>();
 
+        // check for duplicates and how many time they appear while storing them in dupsMap
         for (String product : products) {
             dupsMap.put(product.trim(), dupsMap.getOrDefault(product.trim(), 0) + 1);
         }
 
+        // iterate through dupsMap, get the first element and return it
         for (String prod : dupsMap.keySet()) {
 
             if (dupsMap.get(prod.trim()) == 1) {
@@ -30,7 +37,7 @@ public class Question4_FirstUniqueProd {
 
         Question4_FirstUniqueProd q4 = new Question4_FirstUniqueProd();
         String[] myArr = {"Apple","Computer","Apple","Bag"};
-        System.out.println("output: " + q4.firstUniqueProduct(myArr));
+        System.out.println("output: " + q4.firstUniqueProd(myArr));
     }
 }
 
